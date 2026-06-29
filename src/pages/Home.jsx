a426091e2a8c7b33cb4559f95f38aba93c4b6476
cta10 <cta10@example.com>
2026-06-29 10:20:14 +0530

    Change Who We Are image to a neutral church image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ import { HiArrowRight, HiClock } from 'react-icons/hi';
 import HeroSection from '../components/ui/HeroSection';
 import SectionHeader from '../components/ui/SectionHeader';
 import ScrollReveal from '../components/ui/ScrollReveal';
-import whoWeAreImg from '../assets/who-we-are-new.jpeg';
+import whoWeAreImg from '../assets/church-neutral.jpg';
 
 const ministries = [
   { icon: FaChild, name: 'Sunday School', color: 'from-blue-500 to-blue-600' },
